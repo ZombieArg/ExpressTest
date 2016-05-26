6c053daf62facdef6fd0d6f34043a63b515dd467
Eduardo Pan <white_zombie_85@hotmail.com>
2016-05-26 18:51:33 -0300

    Chapter 4 - update Route with the new controllers - Set view - update the layout - create the views (locations, location-info, add review)

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+/*Get 'home' page*/
+module.exports.homeList = function(req, res){
+	res.render('locations-list', {title: 'Home'});
+};
+
+/*Get 'Location info' page*/
+module.exports.locationInfo = function(req, res){
+	res.render('location-info', {title: 'Location info'});
+};
+
+/*Get 'Add review' page*/
+module.exports.addReview = function(req, res){
+	res.render('location-review-form', {title: 'Add Review'});
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+/*var ctrlMain = require('../controllers/main');*/
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-var homepageController = function(req, res, next) {
-  res.render('index', { title: 'Express' });
-}
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+/*Locations*/
+router.get('/', ctrlLocations.homeList);
+router.get('/locations', ctrlLocations.locationInfo);
+router.get('/locations/review/new', ctrlLocations.addReview);
+
+/*Others*/
+router.get('/about', ctrlOthers.about);
+
 
 module.exports = router;
