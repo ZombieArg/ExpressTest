be47d607b896a550c4cdcdf05ea7552f2bb937de
Eduardo Pan <white_zombie_85@hotmail.com>
2016-05-30 13:15:35 -0300

    Chapter 4 - Moving data from view to controller locations-list, location-info, add new review, others - Create/include/use Mixing

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -8,6 +8,7 @@ module.exports.homeList = function(req, res){
 			title: 'Loc8r',
 			strapline: 'Find places to work with wifi near you!'
 		},
+		sidebar: 'Looking for a wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps  with coffee, cake or a pint? Let Loc8r help you find the place you&rsquo;re looking for.',
 		locations: [{
 			name: 'Starcups',
 			address: '125 High Street, Reading, RG6 1PS',
@@ -32,10 +33,46 @@ module.exports.homeList = function(req, res){
 
 /*Get 'Location info' page*/
 module.exports.locationInfo = function(req, res){
-	res.render('location-info', {title: 'Location info'});
+	res.render('location-info', {
+		title: 'Location info',
+		pageHeader: {
+			title: 'Starcups',
+		},
+		sidebar: {
+			lead: 'Simon&rsquo;s cafe is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+			secondary: 'If you&rsquo;ve been and you like it - or if you don&rsquo;t - please leave a review to help other people just like you.'
+		},
+		location: {
+			name: 'Starcups',
+			address: '125 High Street, Reading, RG6 1PS',
+			rating: 3,
+			facilities: ['Hot Drinks','Food','Premium wifi'],
+			coordinates: {
+				latitude: '51.455041',
+				longitude: '-0.9690884'
+			},
+			openDays: ['Monday - Friday : 7:00am - 7:00pm','Saturday : 8:00am - 5:00pm', 'Sunday : Closed']
+		},
+		reviews:[{
+			author: 'Eduardo Pan',
+			rating: 4,
+			commentDate: '14 July 2016',
+			commentText: 'What a great place. I can&rsquo;t say enough good thing about it.'
+		},{
+			author: 'Sigmund Freud',
+			rating: 2,
+			commentDate: '17 July 2016',
+			commentText: 'It was okay. Coffe sucks, but the wifi was fast.'
+		}]
+	});
 };
 
 /*Get 'Add review' page*/
 module.exports.addReview = function(req, res){
-	res.render('location-review-form', {title: 'Add Review'});
+	res.render('location-review-form', {
+		title: 'Add Review',
+		pageHeader: {
+			title: 'Starcups'
+		}
+	});
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,12 @@
 // JavaScript Document
 
 module.exports.about = function(req, res){
-	res.render('generic-text', {title: 'About'});
+	res.render('generic-text', {
+		title: 'About',
+		pageHeader: {
+			title: 'About',
+			strapline: 'Loc8r was created to help people to find places to sit down and get bit of work done.'
+		},
+		content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
+	});
 };
